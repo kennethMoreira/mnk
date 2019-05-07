7f09ab28a08b9228434dfcf0d3b956b13ec5260c
kennethmoreira <kenneth.moreira@gmail.com>
2019-05-07 20:17:01 +0000

    Home

--- core/app/view/home-view.php
+++ core/app/view/home-view.php
@@ -19,18 +19,39 @@ for($i=$sd;$i<=$ed;$i+=(60*60*24)){
   $ntot+=($sl-($sp+$sr));
   $nsells += $operations[0]->c;
 }
+
 ?>
+
+
   <section class="content-header">
-    <h1>Inventio Max</h1>
-    <h4>Almacen principal: <?php echo StockData::getPrincipal()->name;  ?></h4>
+    <h1>Sistema de Inventario</h1>
+    <h4>Sucursal Favorito: <?php echo StockData::getPrincipal()->name;  ?></h4>
+    <?php
+
+$stocks = StockData::getAll();
+?>
+<div class="col-md-2">
+
+
+	
+	
+
+
+</div>
   </section>
 
     <section class="content">
 <div class="row">
   <div class="col-md-12">
   <a href="./?view=newproduct" class="btn btn-default">Nuevo Producto</a>
-  <a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id; ?>" class="btn btn-default">Inventario Principal</a>
-  <a href="./?view=smallbox&opt=all" class="btn btn-default">Caja chica</a>
+  
+  
+  <a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id; ?>" class="btn btn-default">Sucursal Favorito</a>
+  <?php foreach($stocks as $p):?>
+  <a href="./?view=inventary&stock=<?php echo $p->id;?>" class="btn btn-default"> <?php echo $p->name;?></a>
+	<?php endforeach; ?>
+  
+  <!-- <a href="./?view=smallbox&opt=all" class="btn btn-default">Caja chica</a> -->
 <!--  <a href="./?view=messages&opt=all" class="btn btn-default">Mensajes</a> -->
   </div>
   </div>
@@ -38,74 +59,86 @@ for($i=$sd;$i<=$ed;$i+=(60*60*24)){
 <br>
 <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-glass"></i></span>
+          <!-- <div class="info-box"> -->
+            <!-- <span class="info-box-icon bg-aqua"><i class="fa fa-glass"></i></span> -->
 
-            <div class="info-box-content">
-              <span class="info-box-text">Productos</span>
-              <span class="info-box-number"><?php echo count(ProductData::getAll()); ?><small></small></span>
-            </div>
+            <!-- <div class="info-box-content"> -->
+              <!-- <span class="info-box-text">Productos</span> -->
+              <!-- <span class="info-box-number"><?php echo count(ProductData::getAll()); ?><small></small></span> -->
+            <!-- </div> -->
             <!-- /.info-box-content -->
           </div>
           <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-male"></i></span>
+        <!-- <div class="col-md-3 col-sm-6 col-xs-12"> -->
+          <!-- <div class="info-box"> -->
+            <!-- <span class="info-box-icon bg-red"><i class="fa fa-male"></i></span> -->
 
-            <div class="info-box-content">
-              <span class="info-box-text">Clientes</span>
-              <span class="info-box-number"><?php echo count(PersonData::getClients());?></span>
-            </div>
+            <!-- <div class="info-box-content"> -->
+              <!-- <span class="info-box-text">Clientes</span> -->
+              <!-- <span class="info-box-number"><?php echo count(PersonData::getClients());?></span> -->
+            <!-- </div> -->
             <!-- /.info-box-content -->
-          </div>
+          <!-- </div> -->
           <!-- /.info-box -->
-        </div>
+        <!-- </div> -->
         <!-- /.col -->
 
         <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
+        <!-- <div class="clearfix visible-sm-block"></div> -->
 
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-shopping-cart"></i></span>
+        <!-- <div class="col-md-3 col-sm-6 col-xs-12"> -->
+          <!-- <div class="info-box"> -->
+            <!-- <span class="info-box-icon bg-green"><i class="fa fa-shopping-cart"></i></span> -->
 
-            <div class="info-box-content">
-              <span class="info-box-text">Ventas del mes</span>
-              <span class="info-box-number"><?php echo $nsells;?></span>
-            </div>
+            <!-- <div class="info-box-content"> -->
+              <!-- <span class="info-box-text">Ventas del mes</span> -->
+              <!-- <span class="info-box-number"><?php echo $nsells;?></span> -->
+            <!-- </div> -->
             <!-- /.info-box-content -->
-          </div>
+          <!-- </div> -->
           <!-- /.info-box -->
-        </div>
+        <!-- </div> -->
         <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="fa fa-area-chart"></i></span>
+        <!-- <div class="col-md-3 col-sm-6 col-xs-12"> -->
+          <!-- <div class="info-box"> -->
+            <!-- <span class="info-box-icon bg-yellow"><i class="fa fa-area-chart"></i></span> -->
 
-            <div class="info-box-content">
-              <span class="info-box-text">Ingresos del Mes</span>
-              <span class="info-box-number"><?php echo Core::$symbol; ?> <?php echo number_format($ntot,2,".",",");?></span>
-            </div>
+            <!-- <div class="info-box-content"> -->
+              <!-- <span class="info-box-text">Ingresos del Mes</span> -->
+              <!-- <span class="info-box-number"><?php echo Core::$symbol; ?> <?php echo number_format($ntot,2,".",",");?></span> -->
+            <!-- </div> -->
             <!-- /.info-box-content -->
-          </div>
+          <!-- </div> -->
           <!-- /.info-box -->
-        </div>
+        <!-- </div> -->
         <!-- /.col -->
-      </div>
+      <!-- </div> -->
       <!-- /.row -->
 
-      <div class="row">
-        <div class="col-md-12">
-          <div class="box box-primary">
+      <!-- <div class="row"> -->
+        <!-- <div class="col-md-12"> -->
+          <!-- <div class="box box-primary"> -->
             <!-- /.box-header -->
-            <div class="box-body">
-              <div class="row">
-                <div class="col-md-12">
-                  <p class="text-center">
-                    <strong>Balance de los ultimos 30 dias</strong>
-                  </p>
+            <!-- <div class="box-body"> -->
+              <!-- <div class="row"> -->
+                <!-- <div class="col-md-12"> -->
+                  <!-- <p class="text-center"> -->
+                    <!-- <strong>Balance de los ultimos 30 dias</strong> -->
+                  <!-- </p> -->
+                  <div class = "row">
+                  <div class = "col-md-12">
+                  
+                           <?php
+                   include('inventaries-view.php');
+
+?>
+                  </div>
+                  </div>
+
+
+<!-- view=inventaries -->
 
 
 
@@ -113,36 +146,34 @@ for($i=$sd;$i<=$ed;$i+=(60*60*24)){
 
 
 
-
-
-<?php 
-  $dateB = new DateTime(date('Y-m-d')); 
-  $dateA = $dateB->sub(DateInterval::createFromDateString('30 days'));
-  $sd= strtotime(date_format($dateA,"Y-m-d"));
-  $ed = strtotime(date("Y-m-d"));
+<!-- <?php 
+  // $dateB = new DateTime(date('Y-m-d')); 
+  // $dateA = $dateB->sub(DateInterval::createFromDateString('30 days'));
+  // $sd= strtotime(date_format($dateA,"Y-m-d"));
+  // $ed = strtotime(date("Y-m-d"));
 
 ?>
 <div id="graph" class="animate" data-animate="fadeInUp" ></div>
 <script>
 
 <?php 
-echo "var c=0;";
-echo "var dates=Array();";
-echo "var data=Array();";
-echo "var total=Array();";
-for($i=$sd;$i<=$ed;$i+=(60*60*24)){
-  $operations = SellData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i),2);
-  $res = SellData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i),1);
-  $spends = SpendData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i));
+// echo "var c=0;";
+// echo "var dates=Array();";
+// echo "var data=Array();";
+// echo "var total=Array();";
+// for($i=$sd;$i<=$ed;$i+=(60*60*24)){
+  // $operations = SellData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i),2);
+  // $res = SellData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i),1);
+  // $spends = SpendData::getGroupByDateOp(date("Y-m-d",$i),date("Y-m-d",$i));
 //  echo $operations[0]->t;
-  $sr = $res[0]->tot!=null?$res[0]->tot:0;
-  $sl = $operations[0]->t!=null?$operations[0]->t:0;
-  $sp = $spends[0]->t!=null?$spends[0]->t:0;
-  echo "dates[c]=\"".date("Y-m-d",$i)."\";";
-  echo "data[c]=".($sl-($sp+$sr)).";";
-  echo "total[c]={x: dates[c],y: data[c]};";
-  echo "c++;";
-}
+  // $sr = $res[0]->tot!=null?$res[0]->tot:0;
+  // $sl = $operations[0]->t!=null?$operations[0]->t:0;
+  // $sp = $spends[0]->t!=null?$spends[0]->t:0;
+  // echo "dates[c]=\"".date("Y-m-d",$i)."\";";
+  // echo "data[c]=".($sl-($sp+$sr)).";";
+  // echo "total[c]={x: dates[c],y: data[c]};";
+  // echo "c++;";
+// }
 ?>
 // Use Morris.Area instead of Morris.Line
 Morris.Area({
@@ -154,7 +185,7 @@ Morris.Area({
 }).on('click', function(i, row){
   console.log(i, row);
 });
-</script>
+</script> -->
 
 
 
