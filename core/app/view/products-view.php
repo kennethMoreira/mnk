6ecc37c32e17b2615c87e97474abfb64b45222e2
kennethmoreira <kenneth.moreira@gmail.com>
2019-05-09 14:32:27 -0400

    products list with brand fix

--- core/app/view/products-view.php
+++ core/app/view/products-view.php
@@ -64,11 +64,13 @@ if(count($products)>0){
 			<?php endif;?>
 		</td>
 		<td><?php echo $product->name; ?></td>
+    <td>
     <?php foreach(BrandData::getAll() as $category):?>
       <?php 
       if($product->brand_id!=null&& $product->brand_id==$category->id){ echo $category->name;} 
       
       ?>
+      </td>
 		<td><?php echo $currency; ?> <?php echo number_format($product->price_in,2,'.',','); ?></td>
 		<td><?php echo $currency; ?> <?php echo number_format($product->price_out,2,'.',','); ?></td>
 		<!-- <td><?php if($product->category_id!=null){echo $product->getCategory()->name;}else{ echo "<center>----</center>"; }  ?></td> -->
